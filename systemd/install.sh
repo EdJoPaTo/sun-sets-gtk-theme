e07ef4998bc29b8f7bb2fa7d7d7602086052c7fa
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-04-18 16:40:21 +0200

    chore: switch scripts to /usr/bin/env bash

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 nice cargo install --path ..
 
 mkdir -p ~/.config/systemd/user/
-cp -uv *.service ~/.config/systemd/user/
+cp -uv ./*.service ~/.config/systemd/user/
 
 systemctl --user daemon-reload
 systemctl --user restart sun-sets-gtk-theme.service
