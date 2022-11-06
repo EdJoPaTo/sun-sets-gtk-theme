199bc8dce9aca63e4da56277d4f2136f8403bf00
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-11-06 21:11:36 +0100

    chore(systemd): simplify $name in scripts

--- systemd/user/install.sh
+++ systemd/user/install.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -e
-
-name="sun-sets-gtk-theme"
+set -eu
 
 dir=$(basename "$(pwd)")
 if [ "$dir" == "systemd" ] || [ "$dir" == "user" ]; then
-    echo "run from main directiory like this: ./systemd/user/install.sh"
-    exit 1
+	echo "run from main directiory like this: ./systemd/user/install.sh"
+	exit 1
 fi
 
 # Create config/bin folders
@@ -16,11 +14,11 @@ mkdir -p "$CONFIG_DIR/systemd/user/" "$HOME/.local/bin"
 nice cargo build --release --locked
 
 # systemd
-cp -v "systemd/user/service" "$CONFIG_DIR/systemd/user/$name.service"
+cp -v systemd/user/service "$CONFIG_DIR/systemd/user/sun-sets-gtk-theme.service"
 systemctl --user daemon-reload
 
 # stop, replace and start new version
-systemctl --user stop "$name.service"
-cp -v "target/release/$name" "$HOME/.local/bin"
+systemctl --user stop "sun-sets-gtk-theme.service"
+cp -v "target/release/sun-sets-gtk-theme" "$HOME/.local/bin"
 
-systemctl --user enable --now "$name.service"
+systemctl --user enable --now "sun-sets-gtk-theme.service"
--- systemd/user/remove.sh
+++ systemd/user/remove.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-name="sun-sets-gtk-theme"
-
-systemctl --user disable --now "$name.service"
+systemctl --user disable --now "sun-sets-gtk-theme.service"
 
 CONFIG_DIR=${XDG_CONFIG_DIRS:-"$HOME/.config"}
-rm -f "$CONFIG_DIR/systemd/user/$name.service"
-rm -f "$HOME/.local/bin/$name"
+rm -f "$CONFIG_DIR/systemd/user/sun-sets-gtk-theme.service"
+rm -f "$HOME/.local/bin/sun-sets-gtk-theme"
 
 systemctl --user daemon-reload
